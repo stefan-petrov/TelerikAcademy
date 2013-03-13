be9a712b88d5f377b2107542fa93b2618f9fcea6
Stefan <stefan_v_petrov@mail.bg>
2013-03-13 02:35:03 +0200

    some uploads

---
+++ C#/C# Fundamentals Part1/Operators, Expresions and Statements/OddOrEven.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApplication4
+{
+    class OddOrEven
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please enter a Number: ");
+            int input = int.Parse(Console.ReadLine());            
+            int remainder = input % 2;
+            Console.WriteLine(remainder == 1 ? "The Number is Odd" : "The Number is Even");
+        }
+    }
+}
